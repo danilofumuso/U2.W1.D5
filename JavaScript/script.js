d01c0d3bcf0278e94b594c3b8277dcc20821c578
danilofumuso <danilofumuso@gmail.com>
2024-09-13 17:13:11 +0200

    U2.W1.D5 exam completed v2

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,7 +2,7 @@ const header = document.querySelector("header");
 const button = document.querySelector("header button");
 
 function headerTransition() {
-  if (window.scrollY > 530) {
+  if (window.scrollY > 500) {
     header.classList.add("header-scrolled");
     button.classList.add("button-scrolled");
   } else {
