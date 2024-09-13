03d79c3642b170bca3ef47dab135608f48fd057d
danilofumuso <danilofumuso@gmail.com>
2024-09-13 17:33:14 +0200

    U2.W1.D5 navbar scrollY changed

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,7 +2,7 @@ const header = document.querySelector("header");
 const button = document.querySelector("header button");
 
 function headerTransition() {
-  if (window.scrollY > 500) {
+  if (window.scrollY > 430) {
     header.classList.add("header-scrolled");
     button.classList.add("button-scrolled");
   } else {
