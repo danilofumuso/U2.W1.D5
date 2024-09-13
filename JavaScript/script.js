c9b1108386a429aec09d334ab3d060f193a4cbf2
danilofumuso <danilofumuso@gmail.com>
2024-09-13 16:56:07 +0200

    U2.W1.D5 exam completed

---
+++ JavaScript/script.js
@@ -0,0 +1,14 @@
+const header = document.querySelector("header");
+const button = document.querySelector("header button");
+
+function headerTransition() {
+  if (window.scrollY > 530) {
+    header.classList.add("header-scrolled");
+    button.classList.add("button-scrolled");
+  } else {
+    header.classList.remove("header-scrolled");
+    button.classList.remove("button-scrolled");
+  }
+}
+
+window.addEventListener("scroll", headerTransition);
